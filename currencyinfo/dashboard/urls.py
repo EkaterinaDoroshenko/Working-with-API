3b498a36a08bdee4694b367ac6abecddd630225b
EkaterinaDoroshenko <katerina.d2603@gmail.com>
2024-06-19 10:57:35 +0700

    First Commit

--- currencyinfo/dashboard/urls.py
+++ currencyinfo/dashboard/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('generate_excel/', views.generate_excel_report, name='generate_excel_report'),
     path('home', views.home,name='home'),
     path('checkdollar/', views.checkdollar, name='checkdollar'),
 ]
